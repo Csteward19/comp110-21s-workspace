65e96c263d4957f9875d3da796f3f9bbdafb259f
Charles Steward <csteward@email.unc.edu>
2021-02-05 16:04:59 -0500

    Vaccine Calc Finished

--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -1,6 +1,6 @@
 """A vaccination calculator."""
 
-__author__ = "YOUR PID HERE"
+__author__ = "730404260"
 
 # The datetime data type is imported from the datetime library.
 # A datetime object models a specific date and time.
@@ -18,3 +18,17 @@ from datetime import timedelta
 
 
 # Begin your solution here...
+pop_0 = int(input("Enter population: "))
+print(pop_0)
+doses_0 = int(input("Enter doses administered: "))
+print(doses_0)
+dose_day = int(input("Enter doses per day: "))
+print(dose_day)
+target = int(input("Enter target percent vaccinated: "))
+print(target)
+e = int(((pop_0*(target/100))-(doses_0/2))/(dose_day/2))
+today: datetime = datetime.today()
+e_days: timedelta = timedelta(e)
+future: datetime = today + e_days
+f = future.strftime("%B %d, %Y")
+print('We will reach' , target , '% vaccination' , 'in' , e , 'days, which falls on' , f)
