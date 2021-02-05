aa10d8e9a46d3b3d34efbce143582c5f3a58ee0b
Charles Steward <csteward@email.unc.edu>
2021-02-04 19:18:52 -0500

    Tar Heel Arithmetic Finished

--- exercises/ex01/tar_heels.py
+++ exercises/ex01/tar_heels.py
@@ -1,6 +1,18 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730404260"
 
 
 # Begin your solution here...
+a = int(input("Enter an int: "))
+print(a)
+if a % 2 == 0 and a % 7 == 0:
+    print("TAR HEELS")
+else:
+    if a % 2 == 0:
+        print("TAR")
+    else:
+        if a % 7 == 0:
+            print("HEELS")
+        else:
+            print("CAROLINA")
