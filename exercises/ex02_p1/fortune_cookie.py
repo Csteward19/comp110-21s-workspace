c755494206b88fc7054583a528af5b5cb132fc21
Charles Steward <csteward@email.unc.edu>
2021-02-10 16:57:26 -0500

    finished ex02p1

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -2,17 +2,30 @@
 
 from random import randint
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730404260"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
-    # TODO 2: Print the result of calling your fortune_cookie function.
+    gift: str = fortune_cookie()
+    print(gift)
     print("Now, go spread positive vibes!")
 
 
-# TODO 1: Define your fortune_cookie function here.
+def fortune_cookie() -> str:
+    """The fortune_cookie function"""
+    fortune: int = randint(1, 100)
+    if fortune > 75: 
+        return "Many new changes are coming your way."
+    else: 
+        if fortune > 50: 
+            return "You will receive horrible news."
+        else: 
+            if fortune > 25:
+                return "Love may be around the corner, or five blocks down."
+            else:
+                return "A bright spot is at the end of your tunnel"
 
 
 # Python Idiom for "starting" the program when run as a module.
--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -6,10 +6,22 @@ __author__ = "YOUR 9-DIGIT PID"
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    # TODO 2: Print the response of calling the tar_heels function here.
+    franklin: str = tar_heels(choice)
+    print(franklin)
 
 
-# TODO 1: Define the tar_heels function, and its logic, here.
+def tar_heels(ramses: int) -> str:
+    """The Arithmetic"""
+    if ramses % 2 == 0 and ramses % 7 == 0:
+        return "TAR HEELS"
+    else: 
+        if ramses % 2 == 0:
+            return "TAR"
+        else: 
+            if ramses % 7 == 0:
+                return "HEELS"
+            else: 
+                return "Carolina"
 
 
 if __name__ == "__main__":
