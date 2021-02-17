962d5b03e5e2e910b08ca01773ed25a3e528e608
Charles Steward <csteward@email.unc.edu>
2021-02-17 17:14:27 -0500

    finished ex02

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -18,18 +18,19 @@ def main() -> None:
 
 
 def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int:
-    """The Original"""
+    """The Original."""
     days_left = float(((population * (target / 100)) - (doses / 2)) / (doses_per_day / 2))
     rounded_days_left: int = round(days_left)
     return rounded_days_left
     
+    
 def future_date(time_left: int) -> str:
+    """Gauging time till goal accomplished."""
     today: datetime = datetime.today()
     x_days: timedelta = timedelta(time_left)
     future: datetime = today + x_days
     f = future.strftime("%B %d, %Y")
     return f
-
 
 
 if __name__ == "__main__":
