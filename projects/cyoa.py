b1d60c25263e67c8675a16071cc7d85aaf6af5c7
Charles Steward <csteward@email.unc.edu>
2021-02-28 15:24:35 -0500

    finished pj00

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -3,116 +3,145 @@ from random import randint
 
 def main() -> None:
     """The initial inputs for quiz"""
+    global whitewalker
+    whitewalker = "\U0001F976"
+    global wolf
+    wolf = "\U0001F43A"
+    global lion
+    lion = "\U0001F981"
+    global fish
+    fish = "\U0001F41F"
+    global kraken
+    kraken = "\U0001F419"
+    global rose
+    rose = "\U0001F33A"
+    global sun 
+    sun = "\U00002600"
+    global dragon
+    dragon = "\U0001F409"
+    greet()
     global points
-    points: int = color() * weather() * weapon()
-    print(greet())
+    points = 0
+    color()
+    print(points)
+    if points != 0:
+        points = weather(points)
+        print(points)
+    if points != 0:
+        points = weapon(points)
+        print(points)
+    print(total_points())
 
     
 
 def greet() -> None:
     """Greetings young one."""
-    player: str = str(input("Hello! So you'd like to know what Game of Thrones house you'd be, what is your name?"))
+    global player
+    player = str(input("Hello! So you'd like to know what Game of Thrones house you'd be, what is your name? "))
     return None
     
 
-def color()-> int:
+def color()-> None:
     """Asseses favorite color banner."""
-    fave_color: str = str(input("What is best: red, green, orange, blue, gold, silver, iron, or leave?"))
+    global points
+    fave_color: str = str(input(player + ", what color is best: red, green, orange, blue, gold, silver, iron, or leave? "))
     if fave_color == "red":
-        return 5
+        points = 5
     else:
         if fave_color == "green":
-            return 6
+            points = 6
         else: 
             if fave_color == "orange":
-                return 7
+                points = 7
             else:
                 if fave_color == "blue":
-                    return 8
+                    points = 8
                 else: 
                     if fave_color == "gold":
-                        return 9
+                        points = 9
                     else:
                         if fave_color == "silver":
-                            return 11
+                            points = 11
                         else: 
                             if fave_color == "iron":
-                                return 13
+                                points = 13
                             else:
                                 if fave_color == "leave":
-                                    return 0
-    return randint(1,100)
+                                    points = 0
 
 
-def weather()-> int:
+
+def weather(current_points: int)-> int:
     """Assesses weather preference."""
-    best_weather: str = str(input("What is the best climate?: cold, hot, warm, mild, or leave"))
+    best_weather: str = str(input(player + " ,what is the best climate?: cold, hot, warm, mild, or leave? "))
     if best_weather == "cold":
-        return 143
+        return current_points * 143
     else:
         if best_weather == "hot":
-            return 7
+            return current_points * 7
         else: 
             if best_weather == "warm":
-                return 54
+                return current_points * 54
             else:
                 if best_weather == "mild":
-                    return 40
+                    return current_points * 40
                 else:
                     if best_weather == "leave":
-                        return 0
-    return 
+                        return current_points * 0
+    return 1000
 
 
-def weapon() -> int:
+def weapon(current_points: int) -> int:
     """Assesses participant's preffered weapon in combat."""
-    best_weapon: str = str(input("What is your choice of weapon?: ship, sword, dagger, spear, dragon, or leave"))
+    best_weapon: str = str(input(player + ", what is your choice of weapon?: ship, sword, dagger, spear, dragon, or leave? "))
     if best_weapon == "ship":
-        return 26
+        return current_points * 26
     else: 
         if best_weapon == "sword":
-            return 264
+            return current_points * 264
         else:
             if best_weapon == "dagger":
-                return 18
+                return current_points * 18
             else:
                 if best_weapon == "spear":
-                    return 21
+                    return current_points * 21
                 else: 
                     if best_weapon == "dragon":
-                        return 20
+                        return current_points * 20
                     else: 
                         if best_weapon == "leave":
-                            return 0
-    return 
+                            return current_points * 0
+    return 1000
 
 
 def total_points() -> str:
-    total: int = color() * weather() * weapon()
-    if total % 5 == 0:
-        return "You are from House Targaeryan"
+    total = points
+    if total == 0:
+        return f"You are a Whitewalker. {whitewalker}"
     else:
-        if total % 6 == 0:
-            return "You are from House Tyrell"
+        if total % 5 == 0:
+            return f"You are from House Targaeryan. {dragon}"
         else:
-            if total % 7 == 0:
-                return "You are from House Martell"
+            if total % 6 == 0:
+                return f"You are from House Tyrell. {rose}"
             else:
-                if total % 8 == 0:
-                    return "You are from House Tully"
+                if total % 7 == 0:
+                    return f"You are from House Martell. {sun}"
                 else:
-                    if total % 9 == 0:
-                        return "You are from House Lannister"
+                    if total % 8 == 0:
+                        return f"You are from House Tully. {fish}"
                     else:
-                        if total % 11 == 0:
-                            return "You are from House Stark"
+                        if total % 9 == 0:
+                            return f"You are from House Lannister. {lion}"
                         else:
-                            if total % 13 == 0:
-                                return "You are from House Greyjoy"
+                            if total % 11 == 0:
+                                return f"You are from House Stark. {wolf}"
                             else:
-                                if total == 0:
-                                    return "You are a Whitewalker"
-    return 
+                                if total % 13 == 0:
+                                    return f"You are from House Greyjoy. {kraken}"
+                                
+                                
+    return "hi"
 
 
 
