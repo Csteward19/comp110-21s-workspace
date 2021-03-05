44261fb12210109fb613d91e6c0ca1036343ec80
Charles Steward <csteward@email.unc.edu>
2021-03-05 14:53:22 -0500

    finished avoidfifth

--- exercises/ex03/avoid_fifth.py
+++ exercises/ex03/avoid_fifth.py
@@ -8,16 +8,16 @@ def main() -> None:
     """Entrypoint of the program."""
     vernacular: str = input(str("Type any string: "))
     print(avoid_fifth(vernacular))
-    # Put print statements here to test your function
-    # ex. print(avoid_fifth("hello there"))
+
+
 def avoid_fifth(e_sucks: str) -> str:
     randstr: str = ""
     """The function that offs the letter that shall not be named."""
     for character in e_sucks:
         if character.lower() != "e":
             randstr += character
-
     return randstr
+
 
 if __name__ == "__main__":
     main()
