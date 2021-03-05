3ae9bd84e7dea4f63db996255aaa70179cca34f6
Charles Steward <csteward@email.unc.edu>
2021-03-05 16:32:05 -0500

    finished palindrome

--- exercises/ex03/palindromify.py
+++ exercises/ex03/palindromify.py
@@ -1,13 +1,22 @@
 """EX03 - palindromify function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730404260"
 
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(palindromify("race", false))
-
-
+    print(palindromify("han", True))
+    print(palindromify("race", False))
+def palindromify(pal: str, possible: bool) -> str:
+    """Whether the word can be palindromified."""
+    sample_str: str = pal
+    if possible == True:
+        i: int = len(pal) - 1
+    else:
+        i: int = len(pal) - 2
+    while i >= 0:
+        sample_str += pal[i]
+        i -= 1
+    return sample_str
 if __name__ == "__main__":
     main()
